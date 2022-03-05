135e3a332e46c096456d1b502e7c3dee4efd537a
Seiry Yu <4397354+seiry@users.noreply.github.com>
2022-03-05 14:47:31 +0800

    update

--- caiyun/index.js
+++ caiyun/index.js
@@ -9,12 +9,25 @@ const body = $response.body
 var bodyObj = JSON.parse(body)
 const fakeExpire = 2646216768.302436
 bodyObj.result.is_vip = true
+body.result.is_xy_vip = true
 bodyObj.result.svip_expired_at = fakeExpire
+bodyObj.xy_vip_expire = fakeExpire
 bodyObj.result.wt.vip.svip_expired_at = fakeExpire
 
 
-$done(bodyObj)
+$done(JSON.stringify(bodyObj))
 /*
 ^https:\/\/biz\.caiyunapp\.com\/v2\/user\?app_name\=weather url script-response-body https://raw.githubusercontent.com/seiry/qxscripts/main/caiyun/index.js
 
 */ 
+
+// [
+//     "body",
+//     "parse",
+//     "xy_vip_expire",
+//     "result",
+//     "is_vip",
+//     "vip_expired_at",
+//     "is_xy_vip",
+//     "stringify"
+// ]
